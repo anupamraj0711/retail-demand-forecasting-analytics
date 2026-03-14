214ed0a01a089532567993eeb2cd1b7c1f6dc793
Anupam Raj <anupam99nic@gmail.com>
2026-03-14 12:32:18 +0530

    Push

---
+++ src/dashboard/app.py
@@ -0,0 +1,96 @@
+﻿"""
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load dataset
+df = pd.read_csv("../data/sales_data.csv")
+
+df['ORDERDATE'] = pd.to_datetime(df['ORDERDATE'])
+
+# Title
+st.title("Retail Demand Forecasting Dashboard")
+
+# Show dataset
+st.subheader("Sales Dataset")
+st.write(df.head())
+
+# Sales trend
+st.subheader("Sales Trend Over Time")
+
+daily_sales = df.groupby("ORDERDATE")["SALES"].sum().reset_index()
+
+fig, ax = plt.subplots()
+
+ax.plot(daily_sales["ORDERDATE"], daily_sales["SALES"])
+
+ax.set_xlabel("Date")
+ax.set_ylabel("Sales")
+
+st.pyplot(fig)
+
+# Country filter
+st.subheader("Country Sales Analysis")
+
+country = st.selectbox("Select Country", df["COUNTRY"].unique())
+
+filtered = df[df["COUNTRY"] == country]
+
+st.write(filtered.head())
+"""
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+# Get project root folder
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+
+# Path to dataset
+data_path = os.path.join(BASE_DIR, "data", "sales_data_sample.csv")
+
+# Load dataset
+df = pd.read_csv(data_path, encoding="latin1")
+
+# Convert date column
+df["ORDERDATE"] = pd.to_datetime(df["ORDERDATE"])
+
+# Title
+st.title("Retail Demand Forecasting Dashboard")
+
+# Show dataset
+st.subheader("Sales Dataset")
+st.write(df.head())
+
+# Sales trend
+st.subheader("Sales Trend Over Time")
+
+daily_sales = df.groupby("ORDERDATE")["SALES"].sum().reset_index()
+
+fig, ax = plt.subplots()
+
+ax.plot(daily_sales["ORDERDATE"], daily_sales["SALES"])
+
+ax.set_xlabel("Date")
+ax.set_ylabel("Sales")
+
+st.pyplot(fig)
+
+# Country filter
+st.subheader("Country Sales Analysis")
+
+country = st.selectbox("Select Country", df["COUNTRY"].unique())
+
+filtered = df[df["COUNTRY"] == country]
+
+st.write(filtered.head())
+
+# Product sales
+st.subheader("Top Product Lines")
+
+top_products = df.groupby("PRODUCTLINE")["SALES"].sum()
+
+st.bar_chart(top_products)
+
+filtered = df[df["COUNTRY"] == country]
+st.write(filtered.head())
---
+++ src/store_mongodb.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from pymongo import MongoClient
+
+# Load dataset
+df = pd.read_csv("../data/sales_data_sample.csv", encoding="latin1")
+# Convert date
+df['ORDERDATE'] = pd.to_datetime(df['ORDERDATE'])
+
+# Connect MongoDB
+client = MongoClient("mongodb+srv://anupam4:anupam@cluster0.4evyxt8.mongodb.net/?appName=Cluster0")
+
+db = client["retail_database"]
+
+collection = db["sales_data"]
+
+# Convert dataframe to dictionary
+data = df.to_dict("records")
+
+# Insert into MongoDB
+collection.insert_many(data)
+
+print("Data stored successfully in MongoDB")
